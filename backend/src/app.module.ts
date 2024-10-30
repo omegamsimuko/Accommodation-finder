4c520915c2cdaf9df7b4945ae64e74c9fb506dcd
DBakaya <bsc-32-22@unima.ac.mw>
2024-10-30 12:20:38 +0200

    complete logins with token returns

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,8 +7,15 @@ import { StudentModule } from './student/student.module';
 import { AgentModule } from './agent/agent.module';
 import { PropertyOwnerModule } from './property-owner/property-owner.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), StudentModule, AgentModule, PropertyOwnerModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), StudentModule, AgentModule, PropertyOwnerModule, AuthModule,
+    JwtModule.register({
+      secret: 'ifindSecret',
+      signOptions: {expiresIn: '1hr'},
+      global: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
